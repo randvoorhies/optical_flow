5abe9847c3f5c295459509452d12ac5887d0813c
Randolph Voorhies <rand.voorhies@gmail.com>
2012-05-23 16:37:43 -0700

    Documentation

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <boost/timer.hpp>
 
+// ######################################################################
 class OpticalFlow
 {
   public:
@@ -46,7 +47,9 @@ OpticalFlow::~OpticalFlow()
 }
 
 // ######################################################################
-void drawFeatures(cv::Mat & image, std::vector<cv::Point2f> const & old_corners, std::vector<cv::Point2f> const & new_corners)
+//! Draw the features onto the image, and draw lines between matches
+void drawFeatures(cv::Mat & image,
+    std::vector<cv::Point2f> const & old_corners, std::vector<cv::Point2f> const & new_corners)
 {
   assert(old_corners.size() == new_corners.size());
   for(size_t i=0; i<new_corners.size(); ++i)
@@ -62,7 +65,10 @@ void drawFeatures(cv::Mat & image, std::vector<cv::Point2f> const & old_corners,
   }
 }
 
-void filterPoints(std::vector<cv::Point2f> & p1, std::vector<cv::Point2f> & p2, std::vector<unsigned char> const & status)
+// ######################################################################
+//! Filter out all points from p1 and p2 who's corresponding status byte == 0
+void filterPoints(std::vector<cv::Point2f> & p1, std::vector<cv::Point2f> & p2,
+    std::vector<unsigned char> const & status)
 {
   std::vector<cv::Point2f> p1_filt;     p1_filt.reserve(p1.size());
   std::vector<cv::Point2f> p2_filt; p2_filt.reserve(p1.size());
@@ -71,7 +77,6 @@ void filterPoints(std::vector<cv::Point2f> & p1, std::vector<cv::Point2f> & p2, 
   std::vector<cv::Point2f>::iterator p2_it = p2.begin();
   std::vector<unsigned char>::const_iterator status_it = status.begin();
 
-  // Filter out bad tracks
   while(status_it != status.end())
   {
     if(*status_it > 0)
@@ -88,6 +93,15 @@ void filterPoints(std::vector<cv::Point2f> & p1, std::vector<cv::Point2f> & p2, 
 }
 
 // ######################################################################
+//! Perform forward/backward LK tracking.
+/*! @param key_image The old keyframe image
+    @param curr_image The new incoming image
+    @param corners The old corners, found (e.g. by cv::goodFeaturesToTrack) in the key_image
+    @param new_corners A vector which will be filled with the locations of the corners in curr_image
+    
+    \note This method tries very hard to filter out bad tracks by performing both a forwards, and a backwards LK tracking step.
+          All untrackable points will be removed from the corners vector, so that corners and new_corners will have the same size
+          after trackFeatures() is finished. */
 void trackFeatures(cv::Mat key_image, cv::Mat curr_image, std::vector<cv::Point2f> & corners, std::vector<cv::Point2f> & new_corners)
 {
   cv::Size const searchWindow(15, 15);
@@ -96,27 +110,35 @@ void trackFeatures(cv::Mat key_image, cv::Mat curr_image, std::vector<cv::Point2
 
   if(corners.size() == 0) return;
 
+  // Perform the forward LK step
   std::vector<unsigned char> status(corners.size());
   std::vector<float> error(corners.size());
   calcOpticalFlowPyrLK(key_image, curr_image, corners, new_corners, status, error, searchWindow, 5);
+
+  // Filter out any untrackable points
   filterPoints(corners, new_corners, status);
 
   if(corners.size() == 0) return;
 
+  // Perform the backwards LK step
   std::vector<cv::Point2f> back_corners;
   calcOpticalFlowPyrLK(curr_image, key_image, new_corners, back_corners, status, error, searchWindow, 5);
   
+  // Filter out points that either:
+  // a) Are untrackable by LK (their status byte == 0)
+  // or 
+  // b) Were tracked by the backward LK step to a position that was too far away from the original point. This indicates
+  //    that even though LK though it had a good track, the point path was ambiguous.
   std::vector<cv::Point2f> filt_corners;
   std::vector<cv::Point2f> filt_old_corners;
   std::vector<cv::Point2f>::iterator corners_it = corners.begin();
   std::vector<cv::Point2f>::iterator new_corners_it = new_corners.begin();
   std::vector<cv::Point2f>::iterator back_corners_it = back_corners.begin();
   std::vector<unsigned char>::iterator status_it = status.begin();
-
   while(status_it != status.end())
   {
-    if(*status_it &&
-      sqrt(pow(new_corners_it->y - back_corners_it->y, 2) + pow(new_corners_it->y - back_corners_it->y, 2)) < 10)
+    float const dist = sqrt(pow(new_corners_it->y - back_corners_it->y, 2) + pow(new_corners_it->y - back_corners_it->y, 2));
+    if(*status_it && dist < 10)
     {
       filt_old_corners.push_back(*corners_it);
       filt_corners.push_back(*new_corners_it);
@@ -127,9 +149,7 @@ void trackFeatures(cv::Mat key_image, cv::Mat curr_image, std::vector<cv::Point2
     ++status_it;
   }
   new_corners = filt_corners;
-  corners = filt_old_corners;
-
-
+  corners     = filt_old_corners;
 }
 
 // ######################################################################
@@ -141,6 +161,8 @@ void OpticalFlow::imageCallback(sensor_msgs::ImageConstPtr const & input_img_ptr
     cv_ptr = cv_bridge::toCvShare(input_img_ptr, sensor_msgs::image_encodings::MONO8);
 
     cv::Mat input_image = cv_ptr->image;
+
+    // Scale the incoming image down by 50%
     cv::pyrDown(input_image, input_image);
 
     // Grab a new keyframe whenever we have lost more than half of our tracks
@@ -177,13 +199,13 @@ void OpticalFlow::imageCallback(sensor_msgs::ImageConstPtr const & input_img_ptr
         key_corners_.clear();
     }
 
-    // Draw the results
-    if(key_corners_.size())
-      drawFeatures(input_image, key_corners_, new_corners);
-
+    // Draw the warped keyframe
     if(key_corners_.size())
       cv::imshow("homography", warped_key_image);
 
+    // Draw the features on the input image
+    if(key_corners_.size())
+      drawFeatures(input_image, key_corners_, new_corners);
     cv::imshow("tracker (press key for keyframe)", input_image);
 
     if(cv::waitKey(2) != -1) key_corners_.clear();
