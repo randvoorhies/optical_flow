4c8b56b15dfdb09b9bc6633226f3fc66e049cdb9
Randolph Voorhies <rand.voorhies@gmail.com>
2012-05-24 13:24:15 -0700

    Good settings

--- src/main.cpp
+++ src/main.cpp
@@ -51,6 +51,8 @@ class OpticalFlow
     double matchscore_thresh_param_;
     bool backwards_filter_param_;
     int downsample_times_param_;
+    double backwards_threshold_param_;
+
 
     double avg_time_;
 };
@@ -69,10 +71,11 @@ OpticalFlow::OpticalFlow() :
   
   // Parameters
   ros::NodeHandle private_nh("~");
-  private_nh.param("num_keypoints",     num_keypoints_param_, 50);
-  private_nh.param("matchscore_thresh", matchscore_thresh_param_, 10e8);
-  private_nh.param("backwards_filter",  backwards_filter_param_, true);
-  private_nh.param("downsample_times",  downsample_times_param_, 2);
+  private_nh.param("num_keypoints",       num_keypoints_param_, 50);
+  private_nh.param("matchscore_thresh",   matchscore_thresh_param_, 10e8);
+  private_nh.param("backwards_filter",    backwards_filter_param_, true);
+  private_nh.param("downsample_times",    downsample_times_param_, 2);
+  private_nh.param("backwards_threshold", backwards_threshold_param_, 10.0);
 
   ROS_WARN("Downsample Times>>>> %d", downsample_times_param_);
   ROS_WARN("numkeypoints %d", num_keypoints_param_);
@@ -92,7 +95,6 @@ void OpticalFlow::imuCallback(const sensor_msgs::Imu::ConstPtr &msg)
       msg->orientation.x, 
       msg->orientation.y, 
       msg->orientation.z); 
-
 }
 
 // ######################################################################
@@ -155,14 +157,16 @@ void filterPoints(std::vector<cv::Point2f> & p1, std::vector<cv::Point2f> & p2,
     @param corners The old corners, found (e.g. by cv::goodFeaturesToTrack) in the key_image
     @param new_corners A vector which will be filled with the locations of the corners in curr_image
     @param backwards_filter Should we perform the backwards filtering step? (see note below)
+    @param backwards_threshold How far away can a point travel during the backwards step before it is considered a bad track?
     
     \note This method tries very hard to filter out bad tracks by performing both a forwards, and a backwards LK tracking step.
           All untrackable points will be removed from the corners vector, so that corners and new_corners will have the same size
           after trackFeatures() is finished. */
 void trackFeatures(cv::Mat key_image, cv::Mat curr_image,
-    std::vector<cv::Point2f> & corners, std::vector<cv::Point2f> & new_corners, bool backwards_filter = true)
+    std::vector<cv::Point2f> & corners, std::vector<cv::Point2f> & new_corners,
+    bool backwards_filter = true, double backwards_threshold = 10.0)
 {
-  cv::Size const searchWindow(10, 10);
+  cv::Size const searchWindow(15, 15);
 
   new_corners.resize(corners.size());
 
@@ -198,7 +202,7 @@ void trackFeatures(cv::Mat key_image, cv::Mat curr_image,
   while(status_it != status.end())
   {
     float const dist = sqrt(pow(new_corners_it->y - back_corners_it->y, 2) + pow(new_corners_it->y - back_corners_it->y, 2));
-    if(*status_it && dist < 10)
+    if(*status_it && dist < backwards_threshold)
     {
       filt_old_corners.push_back(*corners_it);
       filt_corners.push_back(*new_corners_it);
@@ -222,6 +226,7 @@ void OpticalFlow::imageCallback(sensor_msgs::ImageConstPtr const & input_img_ptr
 
     cv::Mat input_image = cv_ptr->image;
 
+    // @@@@@@@@@@@@@@@ START TIMING @@@@@@@@@@@@@@@ 
     ros::Time start_time = ros::Time::now();
 
     // Scale the incoming image down
@@ -237,7 +242,9 @@ void OpticalFlow::imageCallback(sensor_msgs::ImageConstPtr const & input_img_ptr
 
     // Track the features from the keyframe to the current frame
     std::vector<cv::Point2f> new_corners;
-    trackFeatures(key_image_, input_image, key_corners_, new_corners, backwards_filter_param_);
+    trackFeatures(key_image_, input_image,
+        key_corners_, new_corners,
+        backwards_filter_param_, backwards_threshold_param_);
 
     cv::Mat warped_key_image;
     if(new_corners.size() < size_t(num_keypoints_param_/2))
@@ -252,39 +259,27 @@ void OpticalFlow::imageCallback(sensor_msgs::ImageConstPtr const & input_img_ptr
 
       // Warp the keyframe image to the new image, and find the squared difference
       cv::warpPerspective(key_image_, warped_key_image, homography, key_image_.size());
-      double matchScore = cv::norm(input_image, warped_key_image, cv::NORM_L2);
+      double matchError = cv::norm(input_image, warped_key_image, cv::NORM_L2);
 
       // If the difference between the warped template and the new frame is too large,
       // then kill the keyframe
-      if(matchScore > matchscore_thresh_param_)
+      if(matchError > matchscore_thresh_param_)
         key_corners_.clear();
-
-      //if(key_corners_.size())
-      //{
-      //  //global_transform_ = homography * global_transform_;
-
-
-      //  // Find the current position of the camera
-      //  //cv::Mat position = global_transform_ * (cv::Mat_<double>(3, 1) << 0, 0, 1);
-      //  //position.at<double>(0,0) -= input_image.cols/2;
-      //  //position.at<double>(0,1) -= input_image.rows/2;
-
-      //  //ROS_INFO("Position: %0.4f %0.4f", position.at<double>(0,0), position.at<double>(0, 1));
-      //}
-      
-
     }
 
     ros::Time end_time = ros::Time::now();
-    double duration_ms = (end_time - start_time).toSec() * 1000.0;
-    if(avg_time_ == -1.0) avg_time_ = duration_ms;
-    else avg_time_ = 0.2 * duration_ms + 0.8 * avg_time_;
-    ROS_INFO("Processing took: %0.4fms",  avg_time_); 
+    // @@@@@@@@@@@@@@@ END TIMING @@@@@@@@@@@@@@@ 
 
+    double duration_ms = (end_time - start_time).toSec() * 1000.0;
+    if(avg_time_ < 0) avg_time_ = duration_ms;
+    else avg_time_ = 0.2 * duration_ms + 0.8 * avg_time_;
+    ROS_INFO("Processing took: %0.4fms (%0.4fhz)",  avg_time_, 1000.0 / avg_time_); 
 
     // Draw the warped keyframe
     if(key_corners_.size())
       cv::imshow("keyframe", warped_key_image);
+    else
+      cv::imshow("keyframe", input_image);
 
     // Draw the features on the input image
     if(key_corners_.size())
