e0a60f5423666a2d5233c08fd6cacb82a736de0d
Randolph Voorhies <rand.voorhies@gmail.com>
2012-05-29 12:03:10 -0700

    About to branch

--- src/main.cpp
+++ src/main.cpp
@@ -48,13 +48,14 @@ class OpticalFlow
 
     Quaternion imu_quat_;
 
+    KeyframeTracker tracker_;
+
     // Parameters
     int num_keypoints_param_;
     double matchscore_thresh_param_;
     bool backwards_filter_param_;
     int downsample_times_param_;
     double backwards_threshold_param_;
-
 
     double avg_time_;
 };
@@ -125,98 +126,99 @@ void drawFeatures(cv::Mat & image,
   }
 }
 
-// ######################################################################
-//! Filter out all points from p1 and p2 with corresponding status byte == 0
-void filterPoints(std::vector<cv::Point2f> & p1, std::vector<cv::Point2f> & p2,
-    std::vector<unsigned char> const & status)
-{
-  std::vector<cv::Point2f> p1_filt;     p1_filt.reserve(p1.size());
-  std::vector<cv::Point2f> p2_filt; p2_filt.reserve(p1.size());
+//// ######################################################################
+////! Filter out all points from p1 and p2 with corresponding status byte == 0
+//void filterPoints(std::vector<cv::Point2f> & p1, std::vector<cv::Point2f> & p2,
+//    std::vector<unsigned char> const & status)
+//{
+//  std::vector<cv::Point2f> p1_filt;     p1_filt.reserve(p1.size());
+//  std::vector<cv::Point2f> p2_filt; p2_filt.reserve(p1.size());
+//
+//  std::vector<cv::Point2f>::iterator p1_it = p1.begin();
+//  std::vector<cv::Point2f>::iterator p2_it = p2.begin();
+//  std::vector<unsigned char>::const_iterator status_it = status.begin();
+//
+//  while(status_it != status.end())
+//  {
+//    if(*status_it > 0)
+//    {
+//      p1_filt.push_back(*p1_it);
+//      p2_filt.push_back(*p2_it);
+//    }
+//    ++p1_it;
+//    ++p2_it;
+//    ++status_it;
+//  }
+//  p1 = p1_filt;
+//  p2 = p2_filt;
+//}
 
-  std::vector<cv::Point2f>::iterator p1_it = p1.begin();
-  std::vector<cv::Point2f>::iterator p2_it = p2.begin();
-  std::vector<unsigned char>::const_iterator status_it = status.begin();
-
-  while(status_it != status.end())
-  {
-    if(*status_it > 0)
-    {
-      p1_filt.push_back(*p1_it);
-      p2_filt.push_back(*p2_it);
-    }
-    ++p1_it;
-    ++p2_it;
-    ++status_it;
-  }
-  p1 = p1_filt;
-  p2 = p2_filt;
-}
-
-// ######################################################################
-//! Perform forward/backward LK tracking.
-/*! @param key_image The old keyframe image
-    @param curr_image The new incoming image
-    @param corners The old corners, found (e.g. by cv::goodFeaturesToTrack) in the key_image
-    @param new_corners A vector which will be filled with the locations of the corners in curr_image
-    @param backwards_filter Should we perform the backwards filtering step? (see note below)
-    @param backwards_threshold How far away can a point travel during the backwards step before it is considered a bad track?
-    
-    \note This method tries very hard to filter out bad tracks by performing both a forwards, and a backwards LK tracking step.
-          All untrackable points will be removed from the corners vector, so that corners and new_corners will have the same size
-          after trackFeatures() is finished. */
-void trackFeatures(cv::Mat key_image, cv::Mat curr_image,
-    std::vector<cv::Point2f> & corners, std::vector<cv::Point2f> & new_corners,
-    bool backwards_filter = true, double backwards_threshold = 10.0)
-{
-  cv::Size const searchWindow(15, 15);
-
-  new_corners.resize(corners.size());
-
-  if(corners.size() == 0) return;
-
-  // Perform the forward LK step
-  std::vector<unsigned char> status(corners.size());
-  std::vector<float> error(corners.size());
-  calcOpticalFlowPyrLK(key_image, curr_image, corners, new_corners, status, error, searchWindow, 5);
-
-  // Filter out any untrackable points
-  filterPoints(corners, new_corners, status);
-
-  if(corners.size() == 0) return;
-
-  if(backwards_filter == false) return;
-
-  // Perform the backwards LK step
-  std::vector<cv::Point2f> back_corners;
-  calcOpticalFlowPyrLK(curr_image, key_image, new_corners, back_corners, status, error, searchWindow, 5);
-  
-  // Filter out points that either:
-  // a) Are untrackable by LK (their status byte == 0)
-  // or 
-  // b) Were tracked by the backward LK step to a position that was too far away from the original point. This indicates
-  //    that even though LK though it had a good track, the point path was ambiguous.
-  std::vector<cv::Point2f> filt_corners;
-  std::vector<cv::Point2f> filt_old_corners;
-  std::vector<cv::Point2f>::iterator corners_it = corners.begin();
-  std::vector<cv::Point2f>::iterator new_corners_it = new_corners.begin();
-  std::vector<cv::Point2f>::iterator back_corners_it = back_corners.begin();
-  std::vector<unsigned char>::iterator status_it = status.begin();
-  while(status_it != status.end())
-  {
-    float const dist = sqrt(pow(new_corners_it->y - back_corners_it->y, 2) + pow(new_corners_it->y - back_corners_it->y, 2));
-    if(*status_it && dist < backwards_threshold)
-    {
-      filt_old_corners.push_back(*corners_it);
-      filt_corners.push_back(*new_corners_it);
-    }
-    ++corners_it;
-    ++new_corners_it;
-    ++back_corners_it;
-    ++status_it;
-  }
-  new_corners = filt_corners;
-  corners     = filt_old_corners;
-}
+//// ######################################################################
+////! Perform forward/backward LK tracking.
+///*! @param key_image The old keyframe image
+//    @param curr_image The new incoming image
+//    @param corners The old corners, found (e.g. by cv::goodFeaturesToTrack) in the key_image
+//    @param new_corners A vector which will be filled with the locations of the corners in curr_image
+//    @param backwards_filter Should we perform the backwards filtering step? (see note below)
+//    @param backwards_threshold How far away can a point travel during the backwards step before it is considered a bad track?
+//    
+//    \note This method tries very hard to filter out bad tracks by performing
+//          both a forwards, and a backwards LK tracking step.  All untrackable points
+//          will be removed from the corners vector, so that corners and new_corners
+//          will have the same size after trackFeatures() is finished. */
+//void trackFeatures(cv::Mat key_image, cv::Mat curr_image,
+//    std::vector<cv::Point2f> & corners, std::vector<cv::Point2f> & new_corners,
+//    bool backwards_filter = true, double backwards_threshold = 10.0)
+//{
+//  cv::Size const searchWindow(15, 15);
+//
+//  new_corners.resize(corners.size());
+//
+//  if(corners.size() == 0) return;
+//
+//  // Perform the forward LK step
+//  std::vector<unsigned char> status(corners.size());
+//  std::vector<float> error(corners.size());
+//  calcOpticalFlowPyrLK(key_image, curr_image, corners, new_corners, status, error, searchWindow, 5);
+//
+//  // Filter out any untrackable points
+//  filterPoints(corners, new_corners, status);
+//
+//  if(corners.size() == 0) return;
+//
+//  if(backwards_filter == false) return;
+//
+//  // Perform the backwards LK step
+//  std::vector<cv::Point2f> back_corners;
+//  calcOpticalFlowPyrLK(curr_image, key_image, new_corners, back_corners, status, error, searchWindow, 5);
+//  
+//  // Filter out points that either:
+//  // a) Are untrackable by LK (their status byte == 0)
+//  // or 
+//  // b) Were tracked by the backward LK step to a position that was too far away from the original point. This indicates
+//  //    that even though LK though it had a good track, the point path was ambiguous.
+//  std::vector<cv::Point2f> filt_corners;
+//  std::vector<cv::Point2f> filt_old_corners;
+//  std::vector<cv::Point2f>::iterator corners_it = corners.begin();
+//  std::vector<cv::Point2f>::iterator new_corners_it = new_corners.begin();
+//  std::vector<cv::Point2f>::iterator back_corners_it = back_corners.begin();
+//  std::vector<unsigned char>::iterator status_it = status.begin();
+//  while(status_it != status.end())
+//  {
+//    float const dist = sqrt(pow(new_corners_it->y - back_corners_it->y, 2) + pow(new_corners_it->y - back_corners_it->y, 2));
+//    if(*status_it && dist < backwards_threshold)
+//    {
+//      filt_old_corners.push_back(*corners_it);
+//      filt_corners.push_back(*new_corners_it);
+//    }
+//    ++corners_it;
+//    ++new_corners_it;
+//    ++back_corners_it;
+//    ++status_it;
+//  }
+//  new_corners = filt_corners;
+//  corners     = filt_old_corners;
+//}
 
 // ######################################################################
 void OpticalFlow::imageCallback(sensor_msgs::ImageConstPtr const & input_img_ptr)
@@ -238,15 +240,20 @@ void OpticalFlow::imageCallback(sensor_msgs::ImageConstPtr const & input_img_ptr
     // Grab a new keyframe whenever we have lost more than half of our tracks
     if(key_corners_.size() < size_t(num_keypoints_param_ / 2))
     {
-      cv::goodFeaturesToTrack(input_image, key_corners_, num_keypoints_param_, 0.01, 10);
-      key_image_ = input_image.clone();
+      tracker_.setKeyframe(input_image.clone());
+      //cv::goodFeaturesToTrack(input_image, key_corners_, num_keypoints_param_, 0.01, 10);
+      //key_image_ = input_image.clone();
     }
 
-    // Track the features from the keyframe to the current frame
+    tracker_.trackFrame(input_image);
     std::vector<cv::Point2f> new_corners;
-    trackFeatures(key_image_, input_image,
-        key_corners_, new_corners,
-        backwards_filter_param_, backwards_threshold_param_);
+    tracker_.getFeatures(key_corners_, new_corners);
+
+    // Track the features from the keyframe to the current frame
+    //std::vector<cv::Point2f> new_corners;
+    //trackFeatures(key_image_, input_image,
+    //    key_corners_, new_corners,
+    //    backwards_filter_param_, backwards_threshold_param_);
 
     cv::Mat warped_key_image;
     if(new_corners.size() < size_t(num_keypoints_param_/2))
@@ -257,16 +264,16 @@ void OpticalFlow::imageCallback(sensor_msgs::ImageConstPtr const & input_img_ptr
     else
     {
       // Find the homography between the last frame and the current frame
-      cv::Mat homography = cv::findHomography(key_corners_, new_corners, CV_RANSAC);
+      //cv::Mat homography = cv::findHomography(key_corners_, new_corners, CV_RANSAC);
 
-      // Warp the keyframe image to the new image, and find the squared difference
-      cv::warpPerspective(key_image_, warped_key_image, homography, key_image_.size());
-      double matchError = cv::norm(input_image, warped_key_image, cv::NORM_L2);
+      //// Warp the keyframe image to the new image, and find the squared difference
+      //cv::warpPerspective(key_image_, warped_key_image, homography, key_image_.size());
+      //double matchError = cv::norm(input_image, warped_key_image, cv::NORM_L2);
 
-      // If the difference between the warped template and the new frame is too large,
-      // then kill the keyframe
-      if(matchError > matchscore_thresh_param_)
-        key_corners_.clear();
+      //// If the difference between the warped template and the new frame is too large,
+      //// then kill the keyframe
+      //if(matchError > matchscore_thresh_param_)
+      //  key_corners_.clear();
     }
 
     ros::Time end_time = ros::Time::now();
@@ -277,11 +284,11 @@ void OpticalFlow::imageCallback(sensor_msgs::ImageConstPtr const & input_img_ptr
     else avg_time_ = 0.2 * duration_ms + 0.8 * avg_time_;
     ROS_INFO("Processing took: %0.4fms (%0.4fhz)",  avg_time_, 1000.0 / avg_time_); 
 
-    // Draw the warped keyframe
-    if(key_corners_.size())
-      cv::imshow("keyframe", warped_key_image);
-    else
-      cv::imshow("keyframe", input_image);
+    //// Draw the warped keyframe
+    //if(key_corners_.size())
+    //  cv::imshow("keyframe", warped_key_image);
+    //else
+    //  cv::imshow("keyframe", input_image);
 
     // Draw the features on the input image
     if(key_corners_.size())
